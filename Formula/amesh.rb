afead39ad1b231f6c7af6552781299c21583b3f8
Ayanel CI <otiai10+ayanel-ci@gmail.com>
2021-06-01 09:57:52 +0000

    Update amesh: v1.2.7

--- Formula/amesh.rb
+++ Formula/amesh.rb
@@ -4,9 +4,9 @@ class Amesh < Formula
   homepage "https://github.com/otiai10/amesh"
 
   # {{{
-  version "1.2.5"
-  url "https://github.com/otiai10/amesh/releases/download/v1.2.5/amesh-darwin.v1.2.5.zip"
-  sha256 "050065c89fc6373de1bfbec0d7d75fe12d06d722d1c977da71a49a2f395c8324"
+  version "1.2.7"
+  url "https://github.com/otiai10/amesh/releases/download/v1.2.7/amesh-darwin.v1.2.7.zip"
+  sha256 "ddcba8ee572f1454b18abcc4dad2199bb01d60d614a20b01208b987b4d9e186c"
   # }}}
 
   def install
